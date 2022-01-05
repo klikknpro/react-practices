7a493a564ff1bdcadf14940bfb1ab38b4b8262a6
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-01-05 10:45:49 +0100

    Refactor correctly

--- src/components/MainButton.js
+++ src/components/MainButton.js
@@ -1,5 +1,10 @@
 function mainButton(props) {
-  return <button onClick={props.onClick}>Click Me</button>;
+  const showText = () => {
+    const mainText = document.querySelector(".mainText");
+    mainText.textContent = props.textToShow;
+  };
+
+  return <button onClick={showText}>Click Me</button>;
 }
 
 export default mainButton;
